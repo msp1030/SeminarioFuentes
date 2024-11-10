7219a49c245168ccea8b7fe7bd10057935f2d329
msp1030 <msp1030@alu.ubu.es>
2024-11-10 17:27:53 +0100

    Carga de datos

--- script.R
+++ script.R
@@ -2,14 +2,29 @@
 library(airqualityES)
 library(tidyverse)
 
-#conjuntos de datos
+#carga de datos
+
+zonas_verdes <- pxR::read.px("data/zonas_verdes.px")$DATA
+
+alzheimer <- pxR::read.px("data/alzheimer_total.px")$DATA
 
 data("airqES")
-#View(airqES)
 
-airqES[]
+#Modificacion Tablas
+
+'''
+Valores Límites de los Contaminantes:
+PM10 = 40
+PM2,5 = 25
+Pb = 0,5
+As = 6
+Cd = 5
+Ni = 20
+B(a)P = 1
+
+'''
 
 calidad_aire <- airqES %>%
   filter(year == 2018) %>%
-  mutate(media_dias = apply(.[, c(8:38)], 1, mean, na.rm = TRUE))%>%
-  select(month, province, station_id, pollutant, media_dias) 
+  mutate(media_mensual = apply(.[, c(8:38)], 1, mean, na.rm = TRUE))%>%
+  select(month, province, station_id, pollutant, media_mensual) 
